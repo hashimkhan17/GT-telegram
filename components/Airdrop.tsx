f58dbf4b6d468219858f333057bfec56369ca2ae
Hamza Shabbir <hamzadevtech01@gmail.com>
2024-08-02 20:21:15 +0500

    Update Airdrop.tsx

--- components/Airdrop.tsx
+++ components/Airdrop.tsx
@@ -1,43 +1,53 @@
 'use client'
 
-import { useEffect, useCallback, useRef } from 'react';
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import IceCube from '@/icons/IceCube';
 import { useGameStore } from '@/utils/game-mechaincs';
+import { dailyReward, friends, iceToken, telegram, tonWallet, twitter, youtube } from '@/images';
+import { formatNumber } from '@/utils/ui';
+import { earnData } from '@/utils/consts';
+import IceCoin from '@/icons/IceCoin';
 
-export function AutoIncrement() {
-  const {
-    lastClickTimestamp,
-    profitPerHour,
-    pointsPerClick,
-    energy,
-    maxEnergy,
-    incrementPoints,
-    incrementEnergy
-  } = useGameStore();
+export default function Airdrop() {
+    const { points, incrementPoints } = useGameStore();
+    const [selectedTask, setSelectedTask] = useState<any | null>(null);
+    const [completedTasks, setCompletedTasks] = useState<Set<string>>(new Set());
 
-  // Use a ref to store the latest values without causing re-renders
-  const stateRef = useRef({ profitPerHour, pointsPerClick, lastClickTimestamp });
+    const handleTaskComplete = (task: any) => {
+        incrementPoints(task.tokens);
+        setCompletedTasks(prev => new Set(prev).add(task.title));
+    };
 
-  // Update the ref when these values change
-  useEffect(() => {
-    stateRef.current = { profitPerHour, pointsPerClick, lastClickTimestamp };
-  }, [profitPerHour, pointsPerClick, lastClickTimestamp]);
+    return (
+        <div className="bg-black flex justify-center">
+            <div className="w-full bg-black text-white h-screen font-bold flex flex-col max-w-xl">
+                <div className="flex-grow mt-4 bg-[#f3ba2f] rounded-t-[48px] relative top-glow z-0">
+                    <div className="absolute top-[2px] left-0 right-0 bottom-0 bg-[#1d2025] rounded-t-[46px] px-4 py-6 overflow-y-auto">
+                        <div className="relative min-h-full pb-20">
+                            <div className="flex justify-center mb-4">
+                                <Image src={iceToken} alt="Ice Token" width={96} height={96} className="rounded-lg mr-2" />
+                            </div>
+                            <h1 className="text-2xl text-center mb-4">Airdrop Tasks</h1>
+                            <p className="text-gray-300 text-center mb-4 font-normal">Get ready for upcoming tasks! Soon, you&apos;ll see a list of challenges below. Complete them to qualify for the Airdrop.</p>
+                            <h2 className="text-base mt-8 mb-4">Tasks list</h2>
+                            <div
+                                className="flex justify-between items-center bg-[#319ee0] rounded-lg p-4 cursor-pointer"
+                            >
+                                <div className="flex items-center">
+                                    <Image src={tonWallet} alt="Ton wallet" width={40} height={40} className="rounded-lg mr-2" />
+                                    <div className="flex flex-col">
+                                        <span className="font-medium">Connect your TON wallet</span>
+                                    </div>
+                                </div>
+                            </div>
 
-  const autoIncrement = useCallback(() => {
-    const { profitPerHour, lastClickTimestamp, pointsPerClick } = stateRef.current;
-    const pointsPerSecond = profitPerHour / 3600;
-    const currentTime = Date.now();
 
-    incrementPoints(pointsPerSecond);
-
-    if (!(lastClickTimestamp && ((currentTime - lastClickTimestamp) < 2000))) {
-      incrementEnergy(pointsPerClick);
-    }
-  }, [incrementPoints, incrementEnergy, pointsPerClick]);
-
-  useEffect(() => {
-    const interval = setInterval(autoIncrement, 1000);
-    return () => clearInterval(interval);
-  }, [autoIncrement]);
-
-  return null;
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
